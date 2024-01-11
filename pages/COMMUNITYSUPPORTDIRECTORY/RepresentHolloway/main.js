9b7786d42fa28aedd6803d74e39a7a4d7ba6b126
gsymonds1 <g.symonds0220201@arts.ac.uk>
2024-01-11 18:31:45 +0000

    cleaned up added search n buttons

--- pages/COMMUNITYSUPPORTDIRECTORY/RepresentHolloway/main.js
+++ pages/COMMUNITYSUPPORTDIRECTORY/RepresentHolloway/main.js
@@ -3,39 +3,37 @@ var sheet_name = 'Sheet1';
 var key = "AIzaSyB5b_wv4yQMDoHTCDDZydcbYxLZ5ISrGbQ"
 var url = 'https://sheets.googleapis.com/v4/spreadsheets/' + id + '/values/' + sheet_name + '?alt=json&key=' + key;
 
-
 function processData(rows) {
   var headers = rows[0];
   var content = document.getElementById('content');
   rows.shift();
-  // console.log(headers, rows);
 
-  rows.forEach(row => {
-    console.log(row[4]);
-
-    //content.innerHTML += "<small class='circleText'>" + row[0] + "</small>";
-
+  function displayContent(row) {
     content.innerHTML += "<div class='circleContainer'><small class='circleText'>" + row[0] + "</small></div>"; // circle
     content.innerHTML += "<div class='contentContainer' style='float: left; width: calc(100% - 140px);'><h2><a href='" + row[13] + "' style='color: black; text-decoration: none;'>" + row[5] + "</a></h2><p>" + row[7] + "</p> <img src='" + row[12] + "' style='width: 50px; height: 50px; top: -100px;'> <br><br><br> </div>";
-    
-    // logo image under the description
-content.innerHTML += ""; // Adjust width and height as needed
-
-
-content.innerHTML += "<a href='" + row[13] + "'><img src='" + row[11] + "' style='width: 23%; float: right; vertical-align: top; margin-top: -20vh; object-fit: cover; height: 100px; width: 100px;'></a>";
-
+    content.innerHTML += ""; // Adjust width and height as needed
+    content.innerHTML += "<a href='" + row[13] + "'><img src='" + row[11] + "' style='width: 23%; float: right; vertical-align: top; margin-top: -20vh; object-fit: cover; height: 100px; width: 100px;'></a>";
     content.innerHTML += "<br><br>"
-    //content.innerHTML += "<p>" + "Community/Group: " + row[8] + "</p>";
-    //content.innerHTML += "<p>" + "Location: " + row[7] + "</p>";
-    //content.innerHTML += "<p>" + "Timing: " + row[9] + "</p>";
-    // content.innerHTML += "<hr>";
-    
+  }
+
+  function handleSearch() {
+    var searchInput = document.getElementById('searchInput').value.toLowerCase();
+    content.innerHTML = ""; // Clear previous content
+
+    rows.forEach(row => {
+      if (row[5].toLowerCase().includes(searchInput)) {
+        displayContent(row);
+      }
+    });
+  }
+
+  document.getElementById('searchInput').addEventListener('input', handleSearch);
+
+  rows.forEach(row => {
+    displayContent(row);
   });
-
 }
-
 
 fetch(url)
   .then(response => response.json())
   .then(data => processData(data.values));
-
--- pages/COMMUNITYSUPPORTDIRECTORY/RepresentHolloway/mainHelp.js
+++ pages/COMMUNITYSUPPORTDIRECTORY/RepresentHolloway/mainHelp.js
@@ -3,54 +3,36 @@ var sheet_name = 'Sheet2';
 var key = "AIzaSyB5b_wv4yQMDoHTCDDZydcbYxLZ5ISrGbQ"
 var url = 'https://sheets.googleapis.com/v4/spreadsheets/' + id + '/values/' + sheet_name + '?alt=json&key=' + key;
 
-//https://docs.google.com/spreadsheets/d/1V389rt574p0RbzM2nbrAq0pw-CRCR1hePuGPoBgZ3Z0
 function processData(rows) {
   var headers = rows[0];
   var content = document.getElementById('content');
   rows.shift();
-  // console.log(headers, rows);
+
+  function displayContent(row) {
+    content.innerHTML += "<div class='circleContainer'><small class='circleText'>" + row[0] + "</small></div>";
+    content.innerHTML += "<div class='contentContainer' style='float: left; width: calc(100% - 140px);'><h2><a href='" + row[11] + "' style='color: black; text-decoration: none;'>" + row[5] + "</a></h2><p>" + row[7] + "</p> <img src='" + row[10] + "' style='width: 50px; height: 50px; top: -100px;'> <br><br><br> </div>";
+    content.innerHTML += "<img src='" + row[9] + "' style='width: 23%; float: right; vertical-align: top; margin-top: -20vh; object-fit: cover; height: 100px; width: 100px;'> ";
+    content.innerHTML += "<br><br>";
+  }
+
+  function handleSearch() {
+    var searchInput = document.getElementById('searchInput').value.toLowerCase();
+    content.innerHTML = ""; // Clear previous content
+
+    rows.forEach(row => {
+      if (row[5].toLowerCase().includes(searchInput)) {
+        displayContent(row);
+      }
+    });
+  }
+
+  document.getElementById('searchInput').addEventListener('input', handleSearch);
 
   rows.forEach(row => {
-    console.log(row[4]);
-
-    //content.innerHTML += "<small class='circleText'>" + row[0] + "</small>";
-
-    content.innerHTML += "<div class='circleContainer'><small class='circleText'>" + row[0] + "</small></div>"; // circle
-    content.innerHTML += "<div class='contentContainer' style='float: left; width: calc(100% - 140px);'><h2><a href='" + row[11] + "' style='color: black; text-decoration: none;'>" + row[5] + "</a></h2><p>" + row[7] + "</p> <img src='" + row[10] + "' style='width: 50px; height: 50px; top: -100px;'> <br><br><br> </div>"; // title and description, logoimg
-    
-    // logo image under the description
-content.innerHTML += ""; // Adjust width and height as needed
-
-
-    content.innerHTML += "<img src='" + row[9] + "' style='width: 23%; float: right; vertical-align: top; margin-top: -20vh; object-fit: cover; height: 100px; width: 100px;'> "; // image
-
-    content.innerHTML += "<br><br>"
-    //content.innerHTML += "<p>" + "Community/Group: " + row[8] + "</p>";
-    //content.innerHTML += "<p>" + "Location: " + row[7] + "</p>";
-    //content.innerHTML += "<p>" + "Timing: " + row[9] + "</p>";
-    // content.innerHTML += "<hr>";
-    
+    displayContent(row);
   });
-
 }
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more"; 
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less"; 
-    moreText.style.display = "inline";
-  }
-}
-
 
 fetch(url)
   .then(response => response.json())
   .then(data => processData(data.values));
-
