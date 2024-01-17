fdef17742ad923a50d5212a29f0fb457289bca0b
gsymonds1 <g.symonds0220201@arts.ac.uk>
2024-01-17 21:13:48 +0000

    Final

--- pages/COMMUNITYSUPPORTDIRECTORY/RepresentHolloway/mainHelp copy.js
+++ pages/COMMUNITYSUPPORTDIRECTORY/RepresentHolloway/mainHelp copy.js
@@ -12,10 +12,9 @@ function processData(rows) {
         // Add this line to check if the selected category is present in row[12]
         if (document.getElementById("categoryDropdown").value === "" || row[12].toLowerCase().includes(document.getElementById("categoryDropdown").value.toLowerCase())) {
             content.innerHTML += "<div class='circleContainer'><small class='circleText'>" + row[0] + "</small></div>"; //circle nu
-            content.innerHTML += "<div class='contentContainer' style='width: calc(100% - 100px);'><h2><a href='" + row[11] + "' style='color: black; text-decoration: none;'>" + row[5];
-            // content.innerHTML += "<br><br><br>"
-            content.innerHTML += `</h2><div class="innerTableContent"><p style="flex: 1; margin: 0; ;vertical-align: top;">`+row[7]+`</p><img src="`+row[9]+`" style="flex: 0 0 100px; object-fit: cover; height: 100px; width: 100px;"></div><img src='`+row[10]+`' style=' height: 23px; top: -100px; margin-left:42px'><br><br><br></div>`;
-            content.innerHTML += '<div id="bottomSubPoint"><a href="about.html" target="_blank"><button id="bot_grouplink" class="contentContainer btn buttonMain" style="width:70%">@' + row[5] + '</button></a></div>' //sub
+            content.innerHTML += "<div class='contentContainer' style='float: left; width: calc(100% - 140px);'><h2><a href='" + row[11] + "' style='color: black; text-decoration: none;'>" + row[5] + "</a></h2><p>" + row[7] + "</p> <img src='" + row[10] + "' style=' height: 23px; top: -100px;'> <br><br><br> </div>"; //title. desc.
+            content.innerHTML += "<img src='" + row[9] + "' style='width: 23%; float: right; vertical-align: top; margin-top: -20vh; object-fit: cover; height: 100px; width: 100px;'> "; //side pic
+            content.innerHTML += '<div id="bottomSubPoint"><a href="about.html" target="_blank"><button id="bot_grouplink" class="contentContainer btn buttonMain" style="width:100%">@' + row[5] + '</button></a></div>' //sub
             content.innerHTML += "<br><br>";
         }
     }
@@ -30,6 +29,7 @@ function processData(rows) {
             }
         });
     }
+
 
 
     document.getElementById('searchInput').addEventListener('input', handleSearch);
