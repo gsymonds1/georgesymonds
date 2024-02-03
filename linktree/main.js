54fa148a4586e9a25c679780af24a984910a6d30
gsymonds1 <g.symonds0220201@arts.ac.uk>
2024-02-03 15:05:54 +0000

    linkbox

--- linktree/main.js
+++ linktree/main.js
@@ -14,14 +14,14 @@ function processData(rows) {
         // content.innerHTML += "<div class='contentContainer' style='width: calc(100% - 100px);'><h2><a href='" + row[1] + "' style='text-decoration: none;'>" + row[0];
         // content.innerHTML += "<br><br><br>"
         content.innerHTML += `
-    <div class="innerTableContent">
-        <h2 style="flex: 1; margin: 0; vertical-align: top;">
-            <a href='${row[1]}' style='text-decoration: none;'>${row[0]}</a>
-        </h2><br><br>
-        <img class="secondLine" src="/linktree/white share logo.png" alt="Small Image">
-        <img src="${row[2]}" style="flex: 0 0 100px; object-fit: cover; height: 100px; width: 100px;"><br>
-    </div><br>
-`;
+    <a href='${row[1]}' style='text-decoration: none;'>
+        <div class="innerTableContent">
+            <h2 style="flex: 1; margin: 0; vertical-align: top;">${row[0]}</h2><br><br>
+            <img class="secondLine" src="/linktree/white share logo.png" alt="Small Image">
+            <img src="${row[2]}" style="flex: 0 0 100px; object-fit: cover; height: 100px; width: 100px;"><br>
+        </div>
+    </a><br>`
+
 //content.innerHTML += '<div id="bottomSubPoint"><a href="about.html" target="_blank"><button id="bot_grouplink" class="contentContainer btn buttonMain" style="width:70%">@' + row[5] + '</button></a></div>' //sub
         
 
