a2541ccd30cb9853bfba85349e9fddbdeecaec60
gsymonds1 <g.symonds0220201@arts.ac.uk>
2024-02-05 17:41:04 +0000

    updatehome

--- Portfolio/SLIDESHOW/script.js
+++ Portfolio/SLIDESHOW/script.js
@@ -1,14 +1,16 @@
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
+console.log("s:"+slides.length);
 const thumbnails = document.querySelectorAll('.thumbnail');
+console.log("t:"+thumbnails.length);
 
 function showSlide(index) {
   if (index < 0) {
-    index = slides.length - 1;
+    index = slides.length + 1;
   } else if (index >= slides.length) {
     index = 0;
   }
-
+  console.log("index: "+index)
   slides.forEach((slide, i) => {
     slide.classList.remove('active');
   });
@@ -24,8 +26,9 @@ function changeSlide() {
 // Function to start the slideshow and change images every 3 seconds
 function startSlideshow() {
   showSlide(0);
-  setInterval(changeSlide, 700); // Change the interval to 3000ms (3 seconds)
+  setInterval(changeSlide, 1200); // Change the interval to 3000ms (3 seconds)
 }
+
 
 // Add click event listeners to the thumbnails
 thumbnails.forEach((thumbnail, index) => {
@@ -33,6 +36,7 @@ thumbnails.forEach((thumbnail, index) => {
     showSlide(index);
   });
 });
+
 
 // Start the slideshow when the page is loaded
 window.onload = startSlideshow;
