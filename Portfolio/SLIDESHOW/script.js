080ecf7919a5b5c0fd6c1c08fff246ec2057fdc6
gsymonds1 <g.symonds0220201@arts.ac.uk>
2024-02-21 15:21:23 +0000

    UPDATEWEBSITE

--- Portfolio/SLIDESHOW/script.js
+++ Portfolio/SLIDESHOW/script.js
@@ -1,42 +1,53 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-console.log("s:"+slides.length);
-const thumbnails = document.querySelectorAll('.thumbnail');
-console.log("t:"+thumbnails.length);
+  let currentSlide = 0;
+      const slides = document.querySelectorAll('.slide');
+      // console.log("s:" + slides.length);
+      const thumbnails = document.querySelectorAll('.thumbnail');
+      // console.log("t:" + thumbnails.length);
 
-function showSlide(index) {
-  if (index < 0) {
-    index = slides.length + 1;
-  } else if (index >= slides.length) {
-    index = 0;
-  }
-  console.log("index: "+index)
-  slides.forEach((slide, i) => {
-    slide.classList.remove('active');
-  });
-
-  slides[index].classList.add('active');
-  currentSlide = index;
-}
-
-function changeSlide() {
-  showSlide(currentSlide + 1);
-}
-
-// Function to start the slideshow and change images every 3 seconds
-function startSlideshow() {
-  showSlide(0);
-  setInterval(changeSlide, 1200); // Change the interval to 3000ms (3 seconds)
-}
+      function showSlide(index) {
+        //NEW
+        // Remove 'active' class from all thumbnails
+        thumbnails.forEach((thumbnail) => {
+          thumbnail.classList.remove('active');
+        });
 
 
-// Add click event listeners to the thumbnails
-thumbnails.forEach((thumbnail, index) => {
-  thumbnail.addEventListener('click', () => {
-    showSlide(index);
-  });
-});
+
+        if (index < 0) {
+          index = slides.length + 1;
+        } else if (index >= slides.length) {
+          index = 0;
+        }
+
+        console.log("DAINDEXHERE " + index)
+        // Add 'active' class to the currently selected thumbnail
+        document.getElementById(`thumbnail${index}`).classList.add('active');
+
+        slides.forEach((slide, i) => {
+          slide.classList.remove('active');
+        });
+        slides[index].classList.add('active');
+        currentSlide = index;
+      }
+
+      function changeSlide() {
+        showSlide(currentSlide + 1);
+      }
+
+      // Function to start the slideshow and change images every 3 seconds
+      function startSlideshow() {
+        showSlide(0);
+        setInterval(changeSlide, 1200); // Change the interval to 3000ms (3 seconds)
+      }
 
 
-// Start the slideshow when the page is loaded
-window.onload = startSlideshow;
+      // Add click event listeners to the thumbnails
+      thumbnails.forEach((thumbnail, index) => {
+        thumbnail.addEventListener('click', () => {
+          showSlide(index);
+        });
+      });
+
+
+      // Start the slideshow when the page is loaded
+      window.onload = startSlideshow;
